102fed0e157d4086da66b5bd0bc0f5805067ddd6
Brian Flores <bflores@gmail.com>
2013-10-19 18:01:02 -0400

    initial commit

---
+++ IconHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace TargetCheck
+{
+    class IconHandler : IDisposable
+    {
+        NotifyIcon icon;
+        private delegate void CheckTargetDelegate(int param1, bool param2);
+        private CheckTargetDelegate ctDelegate;
+        Boolean threadChecking = true;
+
+        static Ping ping = new Ping();
+        static IPAddress address = IPAddress.Parse("10.0.0.0");
+        static PingReply reply;
+
+        public IconHandler()
+        {
+            icon = new NotifyIcon();
+        }
+
+        public void Display()
+        {
+            icon.MouseClick += new MouseEventHandler(icon_MouseClick);
+            icon.Icon = System.Drawing.SystemIcons.Information;
+            icon.Text = "Target Ready";
+            icon.Visible = true;
+
+            ctDelegate = new CheckTargetDelegate(this.CheckTarget);
+            ctDelegate.BeginInvoke(0, false, null, null);
+        }
+
+        public void Dispose()
+        {
+            threadChecking = false;
+            icon.Visible = false;
+            icon.Dispose();
+
+            Application.Exit();
+        }
+
+        void icon_MouseClick(object sender, MouseEventArgs args)
+        {
+            // TODO: On right-click, show settings menu
+            // Change target IP address
+            // Change loop period
+            Dispose();
+        }
+
+        private void CheckTarget(int param1, bool param2)
+        {
+            while (threadChecking)
+            {
+                // ping IP
+                reply = ping.Send(address);
+                if (reply.RoundtripTime < 200)
+                {
+                    icon.Icon = TargetCheck.Properties.Resources.iconTrue;
+                }
+                else
+                {
+                    icon.Icon = TargetCheck.Properties.Resources.iconFalse;
+                }
+
+                // wait 
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TargetCheck
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            IconHandler iHandler = new IconHandler();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (iHandler)
+            {
+                iHandler.Display();
+                Application.Run();
+            }
+        }
+    }
+}
